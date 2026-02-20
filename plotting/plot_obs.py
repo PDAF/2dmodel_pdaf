1f88bca5e0787092d144d1058c037b3c195d572b
Lars Nerger <lars.nerger@awi.de>
2026-02-20 09:10:47 +0100

    Add time variable for plotting observations

--- plotting/plot_obs.py
+++ plotting/plot_obs.py
@@ -5,7 +5,7 @@
 #
 # Requires Python 3, netCDF4, Matplotlib and Numpy.
 
-# Usage: ./plot_ncfield.py <filename> <step> <field>
+# Usage: ./plot_obs.py <filename> <step>
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,9 +13,10 @@ import numpy.ma as ma
 import argparse as ap
 import netCDF4 as nc
 
-def read_and_plot(filename):
+def read_and_plot(filename, step):
     ncfile = nc.Dataset(filename)
-    var = ncfile['obs'][0,:,:]
+    istep = int(step)
+    var = ncfile['obs'][istep-1,:,:]
     var = var.reshape(36,18)
     var_m = ma.masked_outside(var,-10.0,10.0)
     plt.imshow(np.transpose(var_m), origin='lower',interpolation='none')
@@ -26,8 +27,9 @@ def read_and_plot(filename):
 if __name__ == "__main__":
     parser = ap.ArgumentParser()
     parser.add_argument('filename')
+    parser.add_argument('step')
     args = parser.parse_args()
     try:
-        read_and_plot(args.filename)
+        read_and_plot(args.filename, args.step)
     except OSError as err:
         print(err)
